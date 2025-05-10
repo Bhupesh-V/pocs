6352a2813f2bc723cf3e55ba35f6248a619628b1
Bhupesh-V <varshneybhupesh@gmail.com>
2025-05-10 12:03:37 +0530

    take main branch from cli, doc updates

--- git/clone-variations/README.md
+++ git/clone-variations/README.md
@@ -1,6 +1,6 @@
 # Clone Variations
 
-The script `clone-variations.sh` POCs different ways to clone a git repository, with the goal of finding the smallest resulting repository size.
+The script `clone-variations.sh` demonstrates different ways to clone a git repository, with the goal of finding the smallest resulting repository size thereby reducing the time it takes to clone a repository.
 
 ## Usage
 
@@ -8,4 +8,43 @@ The script `clone-variations.sh` POCs different ways to clone a git repository, 
 ./clone-variations.sh https://repo-url
 ```
 
+## Requirements
+
+- `Bash`
+- `Git`
+- `parallel`
+
 The script will take a loongish time to run, go listen to some music or something.
+
+## Sample Run
+
+Cloning the gorm repo (9th May, 2025).
+
+```
+./clone-variations.sh https://github.com/go-gorm/gorm.git
+
+Computers / CPU threads / Max jobs to run
+1:local / 8 / 8
+
+Computer:jobs running/jobs completed/%of started jobs/Average seconds to complete
+local:8/0/100%/0.0s Cloning into bare repository 'clones/blobless-shallow-clone'...
+local:8/1/100%/2.0s Cloning into 'clones/shallow-clone'...
+local:7/2/100%/1.0s Cloning into bare repository 'clones/treeless-bare-clone'...
+local:6/3/100%/0.7s Cloning into bare repository 'clones/bare-blobless-clone'...
+local:5/4/100%/0.8s Cloning into bare repository 'clones/treeless-single-branch-clone'...
+local:4/5/100%/0.6s Cloning into 'clones/plain-clone'...
+local:3/6/100%/0.5s Cloning into bare repository 'clones/blobless-single-branch-clone'...
+local:2/7/100%/0.4s Cloning into bare repository 'clones/bare-clone'...
+local:1/8/100%/0.6s Cloning into 'clones/single-branch-clone'...
+local:0/9/100%/0.6s 
+
+112K    clones/blobless-shallow-clone
+892K    clones/treeless-bare-clone
+1.3M    clones/treeless-single-branch-clone
+1.6M    clones/bare-blobless-clone
+1.8M    clones/shallow-clone
+2.8M    clones/blobless-single-branch-clone
+5.8M    clones/bare-clone
+7.0M    clones/plain-clone
+7.5M    clones/single-branch-clone
+```
--- git/clone-variations/clone-variations.sh
+++ git/clone-variations/clone-variations.sh
@@ -2,59 +2,67 @@
 
 # Ensure a repository URL is provided
 if [ -z "$1" ]; then
-  echo "Usage: $0 <repo_url>"
+  echo "Usage: $0 <repo_url> [branch_name]"
   exit 1
 fi
 
 REPO_URL="$1"
+BRANCH_NAME="${2:-main}"  # Default to 'main' if not provided
 BASE_DIR="clones"
+
+echo "Using branch: $BRANCH_NAME"
 
 # remove previous clones
 rm -rf "${BASE_DIR:?}"/*
 
 plain_clone() {
-  local dir="$BASE_DIR/plain-clone"
+  local dir="$BASE_DIR/plain"
   [ -d "$dir" ] || git clone "$REPO_URL" "$dir"
 }
 
 bare_clone() {
-  local dir="$BASE_DIR/bare-clone"
+  local dir="$BASE_DIR/bare"
   [ -d "$dir" ] || git clone --bare "$REPO_URL" "$dir"
 }
 
+bare_blobless_clone() {
+  local dir="$BASE_DIR/bare-blobless"
+  [ -d "$dir" ] || git clone --bare --filter=blob:none "$REPO_URL" "$dir"
+}
+
 shallow_clone() {
-  local dir="$BASE_DIR/shallow-clone"
+  local dir="$BASE_DIR/shallow"
   [ -d "$dir" ] || git clone --depth 1 "$REPO_URL" "$dir"
 }
 
 treeless_bare_clone() {
-  local dir="$BASE_DIR/treeless-bare-clone"
+  local dir="$BASE_DIR/bare-treeless"
   [ -d "$dir" ] || git clone --bare --filter=tree:0 "$REPO_URL" "$dir"
 }
 
 single_branch_clone() {
-  local dir="$BASE_DIR/single-branch-clone"
-  [ -d "$dir" ] || git clone --single-branch --branch master "$REPO_URL" "$dir"
+  local dir="$BASE_DIR/single-branch"
+  [ -d "$dir" ] || git clone --single-branch --branch "$BRANCH_NAME" "$REPO_URL" "$dir"
 }
 
 blobless_shallow_clone() {
-  local dir="$BASE_DIR/blobless-shallow-clone"
+  local dir="$BASE_DIR/blobless-shallow"
   [ -d "$dir" ] || git clone --bare --filter=blob:none --depth 1 "$REPO_URL" "$dir"
 }
 
 blobless_single_branch_clone() {
-  local dir="$BASE_DIR/blobless-single-branch-clone"
-  [ -d "$dir" ] || git clone --bare --filter=blob:none --single-branch --branch master "$REPO_URL" "$dir"
+  local dir="$BASE_DIR/blobless-single-branch"
+  [ -d "$dir" ] || git clone --bare --filter=blob:none --single-branch --branch "$BRANCH_NAME" "$REPO_URL" "$dir"
 }
 
 treeless_single_branch_clone() {
-  local dir="$BASE_DIR/treeless-single-branch-clone"
-  [ -d "$dir" ] || git clone --bare --filter=tree:0 --single-branch --branch master "$REPO_URL" "$dir"
+  local dir="$BASE_DIR/treeless-single-branch"
+  [ -d "$dir" ] || git clone --bare --filter=tree:0 --single-branch --branch "$BRANCH_NAME" "$REPO_URL" "$dir"
 }
 
-export REPO_URL BASE_DIR
-export -f plain_clone bare_clone shallow_clone treeless_bare_clone single_branch_clone blobless_shallow_clone blobless_single_branch_clone treeless_single_branch_clone
-parallel --progress ::: plain_clone bare_clone shallow_clone treeless_bare_clone single_branch_clone blobless_shallow_clone blobless_single_branch_clone treeless_single_branch_clone
+export REPO_URL BRANCH_NAME BASE_DIR
+export -f plain_clone bare_clone bare_blobless_clone shallow_clone treeless_bare_clone single_branch_clone blobless_shallow_clone blobless_single_branch_clone treeless_single_branch_clone
+parallel --progress ::: plain_clone bare_clone bare_blobless_clone shallow_clone treeless_bare_clone single_branch_clone blobless_shallow_clone blobless_single_branch_clone treeless_single_branch_clone
 
 # results
 du -h -d 0 "$BASE_DIR"/* | sort -h
